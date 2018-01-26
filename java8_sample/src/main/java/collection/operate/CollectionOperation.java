b5a1b93d0c0e3476081e972874649f8429840fe5
developma <developer.daisuke.maeno@gmail.com>
2018-01-26 10:36:08 +0900

    Add some codes.

--- java8_sample/src/main/java/collection/operate/CollectionOperation.java
+++ java8_sample/src/main/java/collection/operate/CollectionOperation.java
@@ -1,4 +1,35 @@
 package collection.operate;
 
+import java.util.*;
+
 public class CollectionOperation {
+    public static void main(String[] args) {
+        System.out.println("--- removeIf(...) ---");
+
+        final List<String> list = new ArrayList<>();
+        list.add("Hokkaido");
+        list.add("Kanagawa");
+        list.add("Saitama");
+        list.add("Tokyo");
+        list.add("Osaka");
+        list.add("Kyoto");
+        list.add("Yamagata");
+
+        list.removeIf(s -> s.length() > 5);
+        list.forEach(System.out::println);
+
+        System.out.println("--- replaceAll(...) ---");
+        list.replaceAll(s -> s.toUpperCase());
+        list.forEach(System.out::println);
+
+        System.out.println("--- computeIfAbsent ---");
+        final List<String> strings1 = Arrays.asList("Hokkaido", "Kanagawa", "Saitama", "Tokyo", "Osaka", "Kyoto", "Yamagata");
+        final Map<Integer, List<String>> map = new HashMap<>();
+        strings1.forEach(s -> {
+            final int length = s.length();
+            final List<String> valueList = map.computeIfAbsent(length, key -> new ArrayList<String>());
+            valueList.add(s);
+        });
+        System.out.println(map);
+    }
 }
